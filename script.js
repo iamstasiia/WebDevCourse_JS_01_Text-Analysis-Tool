48d2255a1ca3eff6bcef1244487c2c3d72141adc
Anastasiia <anastasiia.khrypko@dci-student.org>
2024-03-06 14:06:49 +0100

    adding a continuation effect

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { question } from 'readline-sync';
 import {
     inputText,
     pureText,
@@ -7,17 +8,26 @@ import {
 
 
 function textanalyseTool() {
-    let pureTextArray = pureText(inputText());
-    //console.log(pureTextArray);
+    let prolongation = true;
 
-    console.log('Your text consists of',
-    quantityCounter(pureTextArray), 'words.');
+    while (prolongation) {
+        let pureTextArray = pureText(inputText());
+        //console.log(pureTextArray);
     
-    console.log('Different words are used:',
-    differentWordCounter(pureTextArray));
+        console.log('Your text consists of',
+        quantityCounter(pureTextArray), 'words.');
+        
+        console.log('Different words are used:',
+        differentWordCounter(pureTextArray));
+    
+        console.log('The longest word in the text is:',
+        longestWord(pureTextArray));
 
-    console.log('The longest word in the text is:',
-    longestWord(pureTextArray));
+        let userInput = question('\nDo you want to enter another text? (y/n): ');
+            if (userInput.toLowerCase() !== 'y') {
+                prolongation = false;
+            }
+    }
 }
 
 textanalyseTool();
